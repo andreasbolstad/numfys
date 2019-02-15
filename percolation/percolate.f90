4626fe48eb57ef6b10ede0fdda7e639626955a31
Andreas Bolstad <26123972+AnderBomb@users.noreply.github.com>
2019-02-15 11:30:59 +0100

    Made a functioning percolate.f90

--- percolation/percolate.f90
+++ percolation/percolate.f90
@@ -1,6 +1,15 @@
 program percolate
     implicit none
 
+    interface
+        recursive function findroot(s, sites) result(r)
+            integer, intent(in) :: s
+            integer, pointer, intent(inout) :: sites(:)
+            integer :: r
+        end function findroot 
+    end interface 
+
+
     integer, parameter :: wp = kind(1.d0)
 
     integer :: ns, l, n, m
@@ -9,6 +18,13 @@ program percolate
     integer :: i, j, seed_size, temp0, temp1, next
     integer, allocatable :: seed(:)
     real(wp), allocatable :: randnums(:)
+
+    integer, pointer :: sites(:)
+    integer :: s1, s2, r1, r2, rsmall, rbig, rinf
+
+    integer(8) :: np_inf, avg_s
+    real(wp), allocatable :: p_inf(:), p2_inf(:), sm(:)
+
     open(10, file="bonds.txt", status="old", access="sequential", form="formatted", action="read")
     read(10,*) ns, l, n, m
     allocate(bonds(0:2,0:m-1))
@@ -16,7 +32,6 @@ program percolate
         read(10,*) bonds(0,i), bonds(1,i)
     end do
 
-    print*, ns, l, n, m
     call random_seed(size=seed_size)
     allocate(seed(seed_size))
     seed = 100
@@ -33,6 +48,65 @@ program percolate
         bonds(0,j) = bonds(1, next)
     end do
 
+    allocate(p_inf(0:m), p2_inf(0:m), sm(0:m))
+    p_inf(0) = 0.0_wp
+    p_inf(m) = 1.0_wp
+    p2_inf(0) = 0.0_wp
+    p2_inf(m) = 1.0_wp 
+    sm(0) = 1.0_wp
+    sm(m) = 0.0_wp
 
+    np_inf =  0
+    avg_s = n
+    
+    allocate(sites(0:n-1))
+    sites = -1
+
+    do i = 0, m-1
+        s1 = bonds(0, i)
+        s2 = bonds(1, i)
+        r1 = findroot(s1, sites)
+        r2 = findroot(s2, sites)
+        if (r1 /= r2) then
+            avg_s = avg_s + 2 * int(sites(r1), 8) * int(sites(r2), 8)
+            if (sites(r1) < sites(r2)) then
+                rbig = r1
+                rsmall = r2
+            else
+                rbig = r2
+                rsmall = r1
+            end if
+            sites(rbig) = sites(rbig) + sites(rsmall)
+            sites(rsmall) = rbig
+            if (sites(rbig) < sites(rinf)) then
+                rinf = rbig
+            end if
+        end if
+        np_inf = -sites(rinf)
+        p_inf(i+1) = real(np_inf, wp) / real(n, wp)
+        p2_inf(i+1) = p_inf(i+1)**2
+        sm(i+1) = real(avg_s - np_inf**2, wp) / real(n - np_inf, wp) 
+    end do 
+   
+    open(11, file="measurements.txt", status="replace", form="formatted", access="sequential", action="write")
+    write(11,*) ns, l, n, m
+    do i = 0, m
+        write(11,*) p_inf(i), p2_inf(i), sm(i)
+    end do
+
+    deallocate(bonds, seed, randnums, sites, p_inf, sm)
 
 end program percolate
+
+
+recursive function findroot(s, sites) result(r)
+    integer, intent(in) :: s
+    integer, pointer, intent(inout) :: sites(:)
+    integer :: r
+    if (sites(s) < 0) then
+        r = s
+    else
+        r = findroot(sites(s), sites)
+        sites(s) = r
+    end if
+end function findroot
