458a1ef3ccf13fd3168814d8e4abc0b6ab88b4ee
Andreas Bolstad <26123972+AnderBomb@users.noreply.github.com>
2019-02-15 14:48:49 +0100

    Add plotting code for clusters of different activation probabilities

---
+++ percolation/drawcluster.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename = 'clusters.txt'
+f = open(filename, 'r')
+ns, l, n, m = np.fromstring(f.readline(), dtype=int, sep="\t")
+pcts = np.fromstring(f.readline(), sep="\t")
+clusters = np.loadtxt(filename, skiprows=2)
+
+nfiles = len(clusters) 
+clusters = np.reshape(clusters, (nfiles, l, l))
+
+fig, axarr = plt.subplots(2,3)
+
+for i, c in enumerate(clusters):
+    axarr[i//3, i%3].matshow(c)
+
+plt.show()
