699cb7f75fd8a0c7de47faccea01feb91f4bc8b0
Andreas <26123972+AnderBomb@users.noreply.github.com>
2019-05-07 14:47:22 +0200

    Day 2, halfway update

--- exam/montecarlo.py
+++ exam/montecarlo.py
@@ -29,7 +29,7 @@ etaAA = 1.95
 etaAB = 1.90
 etaBB = 0.70
 
-@njit
+# @njit
 def V_create(a):
     VAA = epsAA * ( (gamAA/a)**6 - np.exp(-a/etaAA) )
     VAB = epsAB * ( (gamAB/a)**6 - np.exp(-a/etaAB) )
@@ -73,29 +73,73 @@ def H_create(grid, V):
     return H
 
 
+# def swap(grid):
+    # swap1, swap2 = np.random.randint(L, size=2)
+    # if grid[swap1] == grid[swap2]:
+        # return swap(grid)
+    # return swap1, swap2
+
 def swap(grid):
-    swap1, swap2 = np.random.randint(L, size=2)
-    if grid[swap1] == grid[swap2]:
-        return swap(grid)
-    return swap1, swap2
+    swap1 = 0
+    swap2 = 0
+    while grid[swap1] == grid[swap2]:
+        swap1, swap2 = np.random.randint(L, size=2)
+    grid[swap1], grid[swap2] = grid[swap2], grid[swap1] 
+    return grid
 
-
-
-@njit
+# @njit
 def F_calc(grid, V, kT):
     H = H_create(grid, V)
     E = np.linalg.eigvalsh(H)
-    # F = 
     return -kT * np.sum( np.log(1 + np.exp(-E/kT) ) )
 
 
+def update_grid_find_F(V, kT, grid, timesteps, multirun=True):
+
+    Flist = np.zeros(timesteps) # Debugging
+    # Fmin = 0.0
+    Fprev = 0.0
+
+    n_outer = timesteps // 100 # Number of changes in T (Monte Carlo timesteps)
+    n_inner = timesteps // n_outer
+
+    Tlist = np.logspace(-1, 3, n_outer)
+
+    for i in range(n_outer):
+        rand_compare_W = np.random.rand(n_inner)
+        for j in range(n_inner):
+            oldgrid = grid.copy()
+            grid = swap(grid)
+            
+            F = F_calc(grid, V, kT)
+            accept = True
+            if F > Fprev:
+                W = np.exp( -(F-Fprev) * Tlist[i] )
+                accept = (W > rand_compare_W[j])
+
+            # Swap back if bigger
+            if accept:
+                Flist[i*n_inner + j] = F # Debugging
+                Fprev = F
+                # if F < Fmin:
+                    # Fmin = F
+            else:
+                grid = oldgrid
+                # Swap back
+
+    # mask = np.nonzero(Flist)
+    if not multirun:
+        plt.figure()
+        plt.matshow(grid.reshape((N, M)))
+
+        plt.figure()
+        plt.plot(np.arange(timesteps), Flist, linestyle="", marker=".")
+
+    return grid, F #Fmin
 
 
 ### Main program ###
-def enthalpy_problem(timesteps):
-    a = 1.3 # Aangstroem
-    # a = 0.9
-    kT = 0.1 # 1/beta
+def enthalpy_problem(timesteps_init, a, kT, multirun=False, load=False):
     
     V = V_create(a)
 
@@ -110,61 +154,105 @@ def enthalpy_problem(timesteps):
     FB = F_calc(grid, V, kT)
 
     # F(a) 
-    N = 50
-    F_a = np.zeros(N)
-    xAs = np.linspace(0.1, 0.9, N)
-    # F_a = np.array([0])
-    # xAs = np.array([0.5])
-    for i, xA in enumerate(xAs):
-        grid = grid_create(xA)
+    N = 100
 
-        c = np.logspace(-2, 1, timesteps)
-        rand_compare_W = np.random.rand(timesteps)
-
-        Fprev = 0
-        for j in range(timesteps):        
-
-            swap1, swap2 = swap(grid)
-            grid[swap1], grid[swap2] = grid[swap2], grid[swap1] 
-            
-            F = F_calc(grid, V, kT)
-            # print(F-Fprev)
-            W = np.exp( (F-Fprev) * c[j] )
-
-            accept = (W < rand_compare_W[j])
-
-            # Swap back if bigger
-            if accept:
-                Fprev = F
-            else:
-                # Swap back
-                grid[swap1], grid[swap2] = grid[swap2], grid[swap1] 
-        print("xA", xA, "F", F)
-        F_a[i] = F
-
-    Fdelta = F_a - FA*xAs - FB*(1-xAs)
-
-    plt.figure()
-    plt.plot(xAs, Fdelta)
-    plt.show()
-        
+    ##############
+    # Computations
+    if multirun:
+        F_a = np.zeros(N)
+        xAs = np.linspace(0.1, 0.9, N)
+    else:
+        F_a = np.array([0])
+        xAs = np.array([0.5])
     
-        # print()
-        # print("timesteps", timesteps)
-        # print("xA", xA)
-        # print("Enthalpy", F - FA*xA - FB*(1-xA))
+    if not load: 
+        for i, xA in enumerate(xAs):
+            # timesteps = timesteps_init
+            timesteps = int(timesteps_init * 2*min(xA, (1-xA)))
+            grid = grid_create(xA)
+
+            grid, F = update_grid_find_F(V, kT, grid, timesteps, multirun=multirun)
+
+            print("xA", xA, "F", F, "timsteps", timesteps)
+            F_a[i] = F
+
+        Fdelta = F_a - FA*xAs - FB*(1-xAs)
+        np.save("Fdeltas", Fdelta)
+
+    ##########
+
+    else:
+        Fdelta = np.load("Fdeltas.npy")
+
+    if multirun:
+        plt.figure()
+        plt.plot(xAs, Fdelta)
+    
+# def enthalpy_problem(timesteps_init, a, kT, multirun=False, load=False):
+    
+    # V = V_create(a)
+
+    # # FA
+    # xA_A = 1
+    # grid = grid_create(xA_A)
+    # FA = F_calc(grid, V, kT)
+    
+    # # FB
+    # xA_B = 0
+    # grid = grid_create(xA_B)
+    # FB = F_calc(grid, V, kT)
+
+    # # F(a) 
+    # N = 100
+
+    # ##############
+    # # Computations
+    # if multirun:
+        # F_a = np.zeros(N)
+        # xAs = np.linspace(0.1, 0.9, N)
+    # else:
+        # F_a = np.array([0])
+        # xAs = np.array([0.5])
+    
+    # if not load: 
+        # for i, xA in enumerate(xAs):
+            # # timesteps = timesteps_init
+            # timesteps = int(timesteps_init * 2*min(xA, (1-xA)))
+            # grid = grid_create(xA)
+
+            # grid, F = update_grid_find_F(V, kT, grid, timesteps, multirun=multirun)
+
+            # print("xA", xA, "F", F, "timsteps", timesteps)
+            # F_a[i] = F
+
+        # Fdelta = F_a - FA*xAs - FB*(1-xAs)
+        # np.save("Fdeltas", Fdelta)
+
+    ##########
+
+    else:
+        Fdelta = np.load("Fdeltas.npy")
+
+    if multirun:
+        plt.figure()
+        plt.plot(xAs, Fdelta)
 
 
 if __name__ == "__main__":
     start = time()
 
-    timesteps = 1000
+    timesteps = 3000
 
-    enthalpy_problem(timesteps)     
+    a = 1.3 # Aangstroem
+    # a = 0.9
+    kT = 0.1 # 1/beta
+
+    enthalpy_problem(timesteps, a, kT, multirun=False, load=False)     
 
     end = time()
     print("time", end-start)
     
+    plt.show()
 
 
 
