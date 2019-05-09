1f9a8dd934345e24423032296076e2b84e4f4573
Andreas <26123972+AnderBomb@users.noreply.github.com>
2019-05-09 18:28:09 +0200

    Make better percolation plots

--- percolation/plots.py
+++ percolation/plots.py
@@ -4,13 +4,27 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from scipy.signal import find_peaks
 
+
+#################
+# Settings
+#################
+
+# Choose lattice type
+lattice_type = 4
+lattice_names = [0, 0, 0, "Honeycomb", "Square", 0, "Triangular"]
+print(lattice_names[lattice_type])
+
+
+# Root of number of sites! Copy of this in "runall"-file, change both if changing either
+if lattice_type == 6: # computed triangular with more Ns than the other cases..
+    nsites = [100,  112,  128,  144,  162,  184,  206,  234,  264,  298,  336,  380,  430,  484,546,  616,  696,  784,  886, 1000]
+else:
+    nsites = [100, 130,  166,  216,  278,  360,  464,  600,  774, 1000] 
+
+
+
 data = []
-
-# Root of number of sites! Copy of this in runall, change both if changing either
-nsites = [100, 130,  166,  216,  278,  360,  464,  600,  774, 1000] 
-#nsites = [100,  112,  128,  144,  162,  184,  206,  234,  264,  298,  336,  380,  430,  484,546,  616,  696,  784,  886, 1000]
-
-basefilename = "t3.txt"
+basefilename = ("t%d.txt" % lattice_type)
 for i in nsites:
     data.append(np.loadtxt("data/" + str(i) + "x" + str(i) +  basefilename, skiprows=1))
 
@@ -26,23 +40,27 @@ data = np.array(data)
 # Calculations
 #################
 
+### Linear regression. Finds slope and correlation R^2
 chi = np.array(nsites).astype(int)
 logchi = np.log(chi)
-
 minus_beta_del_nu = np.zeros(nq)
 r = np.zeros(nq)
-
 for i in range(nq):
     logpq = np.log(data[:,i,0])
     minus_beta_del_nu[i], _, r[i], _, _ = linregress(logchi, logpq) 
-
 r2 = np.square(r)
+
+
+### Find first peak of R^2
 idx_r2max = find_peaks(r2, prominence=0.01)[0][0]
+
+
+### Find critical exponents pc
 pc = idx_r2max / (nq-1)
 beta_del_nu = -minus_beta_del_nu[idx_r2max]
 print("beta/nu", 0.1042, beta_del_nu)
 
-idx_smax = np.argmax(data[:,:,1], axis=1)
+idx_smax = np.argmax(data[:,:,1], axis=1) # Index of largest smax values 
 smax = np.zeros(nfiles)
 for i in range(nfiles):
     smax[i] = data[i,idx_smax[i], 1]
@@ -53,7 +71,7 @@ print("gamma/nu", 1.7917, gamma_del_nu)
 
 qmax = idx_smax / (nq-1)
 x = np.log(pc-qmax)
-nu = -linregress(x, logchi)[0]
+nu = -linregress(x, logchi)[0] # Slope of x vs log(chi) gives nu
 
 gamma =  gamma_del_nu * nu
 beta = beta_del_nu * nu
@@ -70,36 +88,49 @@ print("Nu\t", 4/3, "\t", nu)
 #################
 plot = True
 
-x = np.linspace(0, 1, nq)
-lowlim = 0
-highlim = 1
 if plot:
+    x = np.linspace(0, 1, nq)
+    if lattice_type == 3:
+        lowlim = 0.4 # Show smaller region of the plot, 0.4 to 0.8
+        highlim = 0.8
+    if lattice_type == 4:
+        lowlim = 0.3
+        highlim = 0.7
+    if lattice_type == 6:
+        lowlim = 0.2
+        highlim = 0.6
     # Major component probability (p_inf)
     plt.figure()
     plt.xlim(lowlim, highlim)
-    plt.title(r"$p_{\infty}$")
+    plt.xlabel("q")
+    plt.ylabel(r"$p_{\infty}$")
     for i in range(nfiles):
         y = data[i,:,0]
-        plt.plot(x, y, label="N=%s" % nsites[i])
+        plt.plot(x, y, label="N=%s" % int(nsites[i]**2))
     plt.legend()
+    plt.savefig("p_infty%d.pdf" % lattice_type)
 
     # <s>
     plt.figure()
     plt.xlim(lowlim, highlim)
-    plt.title("<s>")
+    plt.xlabel("q")
+    plt.ylabel("<s>")
     for i in range(nfiles):
         y = data[i,:,1]
-        plt.plot(x,y, label="N=%s" % nsites[i])
+        plt.plot(x,y, label="N=%s" % int(nsites[i]**2))
     plt.legend()
+    plt.savefig("s_avg%d.pdf" % lattice_type)
 
     # chi
     plt.figure()
     plt.xlim(lowlim, highlim)
-    plt.title(r"$\chi$")
+    plt.xlabel("q")
+    plt.ylabel(r"$\chi$")
     for i in range(nfiles):
         y = data[i,:,2]
-        plt.plot(x,y, label="N=%s" % nsites[i])
+        plt.plot(x,y, label="N=%s" % int(nsites[i]**2))
     plt.legend()
+    plt.savefig("chi%d.pdf" % lattice_type)
     
     
 plt.show()
